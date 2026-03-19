a40b2655ca07c5a57c37629aaa4bba187956eed9
Johany Quintero <johandquintero@utp.edu.co>
2026-03-19 16:34:30 -0500

    fix: export NotificationBell and other UI components from index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,7 @@ export { FormStepper } from './FormStepper';
 export { SuccessState } from './SuccessState';
 export { ViewModeToggle } from './ViewModeToggle';
 export { CardSkeleton } from './CardSkeleton';
+export { NotificationBell } from './NotificationBell';
+export { ShareButtons } from './ShareButtons';
+export { FormInput, FormSelect, CategorySelector, FormButton, DateTimeInput, ToggleOption, FormSection, FormNavigation } from './FormComponents';
+export { PlanCard, BillingToggle, FeatureHighlight, BusinessBundle } from './PricingComponents';
